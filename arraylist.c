1dafda484f7daa55645a68d562b481772adf1a03
Replit user <>
2023-03-30 16:25:01 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -75,6 +75,7 @@ int get_size(ArrayList * l){
 
 //remove elements
 void clean(ArrayList * l){
+    free(l->data);
     l->capacity = 2;
     l->size = 0;
     l->data = (void **) malloc(sizeof(void*));
